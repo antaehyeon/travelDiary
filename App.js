c0b4e977e651128ec8b376918532e19343fba413
HYEON(iMac) <antaehyeon@live.co.kr>
2019-07-24 08:03:17 +0900

    Feat: Apollo Library 추가 및 서버 테스트
    apollo-boost: Package containing everything you need to set up Apollo Client
    react-apollo: View layer integration for React
    graphql: Also parses your GraphQL queries

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
 import Router from "src/library/router/router.js";
+
+import { Text, View } from "react-native";
 
 export default class App extends Component {
   render() {
